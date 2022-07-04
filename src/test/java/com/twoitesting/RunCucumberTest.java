a9c7f3a107cdb0d6fd42ca5c27ad20938f80870c
Michal Kowalski <michal.kowalski@2itesting.com>
2022-07-04 02:07:01 +0100

    some minor changes and cleanup

--- src/test/java/com/twoitesting/RunCucumberTest.java
+++ src/test/java/com/twoitesting/RunCucumberTest.java
@@ -10,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/twoitesting")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
 
